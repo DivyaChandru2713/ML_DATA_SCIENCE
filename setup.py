89d517cec0efa6302058a73f679baa6810259892
divya <divyachandruu@gmail.com>
2025-07-01 11:12:41 +0530

    my second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#ML_DATA_SCIENCE
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=["numpy","pandas"]
+
+)
